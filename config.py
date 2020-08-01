33540dfbb06c3412f1750846ca07b30353065faf
mattmoormeier <matt@gpctechteam.com>
2020-08-01 12:14:28 -0400

    updated files

---
+++ config.py
@@ -0,0 +1,10 @@
+# Enter your API key
+
+census_key = "d52e1529556a6a29e3b8d855e6122ba7eb8a3826"
+
+
+postg = {
+    "host": "localhost",
+    "user": "postgres",
+    "passwd": "postgres",
+    "db": "SQLHomework"}
